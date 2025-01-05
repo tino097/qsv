785c982f227c21d62ad34b8b28ba02f8aac41bb5
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-01-05 18:55:55 -0500

    tests: add test for issue 2391

--- tests/test_extsort.rs
+++ tests/test_extsort.rs
@@ -53,3 +53,63 @@ fn extsort_csvmode() {
 
     assert_eq!(dos2unix(&sorted_output), dos2unix(&expected_csv));
 }
+
+#[test]
+fn extsort_issue_2391() {
+    let wrk = Workdir::new("extsort_issue_2391").flexible(true);
+    wrk.clear_contents().unwrap();
+
+    let unsorted_csv = wrk.load_test_resource("issue2391-test_ids.csv");
+    wrk.create_from_string("issue2391-test_ids.csv", &unsorted_csv);
+
+    // as git mangles line endings, we need to convert manually to CRLF as per issue 2391
+    // see https://github.com/dathere/qsv/issues/2391
+    // convert LF to CRLF in test file to ensure consistent line endings
+    #[cfg(target_os = "windows")]
+    {
+        let mut cmd = wrk.command("cmd");
+        cmd.args([
+            "/C",
+            "type issue2391-test_ids.csv > issue2391-test_ids.tmp.csv && move /Y \
+             issue2391-test_ids.tmp.csv issue2391-test_ids.csv",
+        ]);
+        wrk.output(&mut cmd);
+    }
+    #[cfg(not(target_os = "windows"))]
+    {
+        let mut cmd = wrk.command("sh");
+        cmd.args([
+            "-c",
+            "sed 's/$/\r/' issue2391-test_ids.csv > issue2391-test_ids.tmp.csv && mv \
+             issue2391-test_ids.tmp.csv issue2391-test_ids.csv",
+        ]);
+        wrk.output(&mut cmd);
+    }
+
+    let mut cmd = wrk.command("extsort");
+    cmd.arg("issue2391-test_ids.csv")
+        .args(["--select", "tc_id"]);
+
+    wrk.assert_success(&mut cmd);
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["pnm", "tc_id", "pc_id"],
+        svec!["405", "139280", "9730000630075"],
+        svec!["405", "139280", "9730000630075"],
+        svec!["405", "139281", "9730000630075"],
+        svec!["131", "139282862", "9730065908379"],
+        svec!["138", "139282863", "9730065908379"],
+        svec!["138", "139282864", "9730065908379"],
+        svec!["405", "139282865", "9730065908379"],
+        svec!["138", "139282866", "9730065908379"],
+        svec!["138", "139282867", "9730065908379"],
+        svec!["138", "139282868", "9730065908379"],
+        svec!["138", "139282869", "9730065908379"],
+        svec!["138", "139282870", "9730065908379"],
+        svec!["138", "139282871", "9730065908379"],
+        svec!["252", "139282", "9730000630075"],
+        svec!["241", "139283", "9730000630075"],
+        svec!["272", "139284", "9730000630075"],
+    ];
+    assert_eq!(got, expected);
+}
