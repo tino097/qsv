02b277c4d88782da26d04d6d567a2e7ff0abc044
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-03-03 12:08:47 -0500

    Revert "refactor: `luau` turn off Luau compiler optimizations on Windows"
    This reverts commit f7d9c5a87f6f9dcb86f3bd8ba83f1b3178fd8792.
    
    I turns out compiler optimization on Windows is not the root cause.
    
    [skip ci]

--- src/cmd/luau.rs
+++ src/cmd/luau.rs
@@ -428,10 +428,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
 
     let debug_enabled = log_enabled!(log::Level::Debug) || log_enabled!(log::Level::Trace);
 
-    // add performance optimizations compiler directives if debug is off and we're not on Windows
-    // as this might be causing concurrent access issues only on Windows as suggested by random
-    // access CI tests that only fails on Windows
-    #[cfg(not(target_os = "windows"))]
+    // add performance optimizations compiler directives if debug is off
     if !debug_enabled {
         main_script = format!("--!optimize 2\n--!native\n{main_script}");
     }
